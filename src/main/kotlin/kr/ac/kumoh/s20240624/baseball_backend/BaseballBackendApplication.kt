e4ac656755a746fc043f25e0392b44026e8012e4
sem1n <songsm1129@gmail.com>
2025-12-04 10:21:02 +0900

    🎉Project 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20240624/baseball_backend/BaseballBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20240624.baseball_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BaseballBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BaseballBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20240624/baseball_backend/BaseballBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20240624.baseball_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BaseballBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
